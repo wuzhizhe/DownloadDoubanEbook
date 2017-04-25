a6f73b9698259f655fd1506d88898506e5e6df8d
wuzhizhe <wuzhizhemu569@gmail.com>
2017-04-25 16:56:35 +0800

    add code

--- js/modify.js
+++ js/modify.js
@@ -1,3 +1,10 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-62654628-2']);
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = 'https://ssl.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; 
+s.parentNode.insertBefore(ga, s);
+
 var url = null;
 
 function callback(details) {
